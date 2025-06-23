036d1c221a43cb16556f65525b4c0119f1f010dc
muhamsh <muhammad-ashraful@outlook.com>
2025-06-24 01:41:19 +0600

    lest have some practices

---
+++ fund/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var a int
+	// declare a variable with static type
+	a = 14
+	// g = 49
+	// fmt.Println(g)
+	fmt.Println(a)
+
+	c := 1213
+	fmt.Println(c)
+
+	f, i, j, k, l := 121, true , "ash", 13.3343 , "wertwe"
+	fmt.Println(f, i, j, k, l)
+}
